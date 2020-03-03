11c7902cb09281c3b4a23f8c3f95e0737c89e26d
Martin Jediny <jedinym@gmail.com>
2020-03-03 13:42:08 +0100

    Started writing tests

--- Test/map/LinearnaTest.java
+++ Test/map/LinearnaTest.java
@@ -5,16 +5,24 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class LinearnaTest {
+class LinearnaTest
+{
+    Linearna a;
+    Linearna b;
+    Linearna c;
+
 
     @BeforeEach
     void setUp()
     {
-
+        a = new Linearna(1,1);
+        b = new Linearna(-3, 6);
+        c = new Linearna(-3, 3);
     }
 
     @Test
     void getKorene()
     {
+
     }
 }
