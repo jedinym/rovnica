ec70abdebaeabeebca8dee68876f900232e85e55
Martin Jediny <jedinym@gmail.com>
2020-03-03 13:15:48 +0100

    Druhy - testovaci commit

--- src/test/Test/RovnicaTest.java
+++ src/test/Test/RovnicaTest.java
@@ -2,10 +2,13 @@ package Test;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import map.Linearna;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class RovnicaTest {
+class RovnicaTest
+{
+    Linearna prva;
 
     @BeforeEach
     void setUp()
