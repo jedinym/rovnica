50ecc4248df904f04c44912546321d83553c01ef
Martin Jediny <jedinym@gmail.com>
2020-03-03 13:25:15 +0100

    Added test files

---
+++ Test/map/LinearnaTest.java
@@ -0,0 +1,20 @@
+package map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinearnaTest {
+
+    @BeforeEach
+    void setUp()
+    {
+
+    }
+
+    @Test
+    void getKorene()
+    {
+    }
+}
